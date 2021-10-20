6303acbb3183a4afc992fcec4d2ded64b62cbfa9
Sebastian Soto <thefirstcoolone@gmail.com>
2021-10-20 08:32:45 -0600

    Add project files.

---
+++ RollOfTheDiceListBox/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.UpdateButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 14
+        Me.ListBox1.Location = New System.Drawing.Point(12, 120)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(520, 256)
+        Me.ListBox1.TabIndex = 0
+        '
+        'UpdateButton
+        '
+        Me.UpdateButton.Location = New System.Drawing.Point(175, 415)
+        Me.UpdateButton.Name = "UpdateButton"
+        Me.UpdateButton.Size = New System.Drawing.Size(75, 23)
+        Me.UpdateButton.TabIndex = 1
+        Me.UpdateButton.Text = "Update"
+        Me.UpdateButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(175, 386)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 23)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(545, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.UpdateButton)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents UpdateButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ RollOfTheDiceListBox/Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form1
+    Sub Count()
+        Dim rowData As String
+        For i = 0 To 10
+            For j = 0 To 10
+                rowData &= CStr(i) & CStr(i).PadLeft(3) & " "
+
+            Next
+            ListBox1.Items.Add(rowData)
+            rowData = " "
+        Next
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
+        Roll()
+    End Sub
+    Sub Roll()
+        Dim currentRoll(12) As Integer
+        Dim linePrint As Integer
+        'Two Random numbers between 1 and 6 and added together and corresponding Array variable is incremented by 1
+        For i = 1 To 1000
+            currentRoll((RollOneDie(5)) + (RollOneDie(5))) += 1
+        Next
+        Console.Write(StrDup(19, " "))
+        Console.WriteLine("Roll Of the Dice")
+        Console.WriteLine(StrDup(55, "="))
+        For i = LBound(currentRoll) To UBound(currentRoll)
+            linePrint += 1
+            If linePrint > 2 Then
+                Console.Write(CStr(i).PadLeft(4) & "|")
+            End If
+            ListBox1.Items.Add(linePrint)
+        Next
+        Console.WriteLine()
+        Console.WriteLine(StrDup(55, "-"))
+        For i = LBound(currentRoll) To UBound(currentRoll)
+            Select Case currentRoll(i)
+                Case = 0
+                    'If 0 then do nothing
+                Case <> 0
+                    Console.Write(CStr(currentRoll(i)).PadLeft(4) & "|")
+            End Select
+        Next
+        Console.WriteLine()
+        Console.WriteLine(StrDup(55, "="))
+        Console.ReadLine()
+
+    End Sub
+    'The RollOneDie() function will return a random number between 1 and a maximum inputed number 
+    'Example, RollOneDie(10) will return a number between 1 and 10
+    Function RollOneDie(max As Integer) As Integer
+        Dim randomDouble As Double
+        Dim randomInteger As Integer
+        Randomize(Now.Millisecond)
+        'This random Numbeer will round down to a whole number
+        randomDouble = System.Math.Floor(CDbl(Rnd() * (max + 1)))
+        randomInteger = CInt(randomDouble)
+        '1 is added to the number to prevent 0's
+        randomInteger += 1
+        Return randomInteger
+    End Function
+End Class
